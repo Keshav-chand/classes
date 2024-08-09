b4b0ff08a8eb621490a5508881f5b9d450b71911
Keshav chand <Keshavchand204@gmail.com>
2024-08-10 00:27:27 +0530

    first commit

---
+++ class.tsx
@@ -0,0 +1,29 @@
+class Pattern {
+    result = (height: number) => {
+        var rows = height;
+        var s = "";
+
+        // First half of the pattern
+        for (var i = 0; i <= rows; i++) {
+            var j = 0;
+            while (j < i) {
+                s += '*';
+                j++;
+            }
+            s += '\n';
+        }
+
+        // Second half of the pattern
+        for (i = rows - 1; i > 0; i--) {
+            for (j = 0; j <= i - 1; j++) {
+                s += '*';
+            }
+            s += '\n';
+        }
+
+        return s;
+    }
+}
+
+var pattern_obj = new Pattern();
+console.log(pattern_obj.result(5));
